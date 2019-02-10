71487397194aed4f68f1cd83adba91a1fe9ad69f
EstebanCarranza <esteban.carranza@outlook.com>
2019-02-10 01:33:05 -0600

    gitignore database

--- app/libraries/Database.php
+++ app/libraries/Database.php
@@ -1,10 +1,10 @@
 <?php
 class database
 {
-    var $host = "localhost-example";
-    var $daba = "database-example";
-    var $user = "user-example";
-    var $pass = "production-example";
+    var $host = "twicky.com.mx";
+    var $daba = "DB_TWICKY";
+    var $user = "produccion";
+    var $pass = "produccion";
 
     public function __constructor()
     {
